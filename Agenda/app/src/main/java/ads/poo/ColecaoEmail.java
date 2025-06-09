91a132db34e1232dbdb3b3e862801133049c3bfd
Victor Lacerda <163481651+victorlcrd@users.noreply.github.com>
2025-06-09 18:44:12 -0300

    Update ColecaoEmail.java

--- Agenda/app/src/main/java/ads/poo/ColecaoEmail.java
+++ Agenda/app/src/main/java/ads/poo/ColecaoEmail.java
@@ -1,65 +1,53 @@
 package ads.poo;
 
-import javax.swing.text.MaskFormatter;
-import java.text.ParseException;
 import java.util.HashMap;
 
-public class ColecaoTelefone {
-    private HashMap<String, String> dados = new HashMap<>();
+public class ColecaoEmail {
 
-    public boolean add(String rotulo, String telefone) {
-        String eR = "^[0-9]+$";
+    private HashMap<String,String> dados = new HashMap<>();
 
-        if (telefone.matches(eR) && dados.get(rotulo) == null) {
-            dados.put(rotulo, telefone);
+
+    public boolean add(String email, String rotulo){
+        String eR = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+        if(email.matches(eR)) {
+            dados.put(rotulo, email);
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
-    public boolean remove(String rotulo) {
-        if (dados.containsKey(rotulo)) {
-            dados.remove(rotulo);
+    public boolean update(String email, String rotulo) {
+        String eR = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+        if (email.matches(eR) && dados.get(email) == null) {
+            dados.put(rotulo, email);
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
-    public boolean update(String rotulo, String telefone) {
-        String eR = "^[0-9]+$";
-        if (telefone.matches(eR) && dados.containsKey(rotulo)) {
-            dados.put(rotulo, telefone);
-            return true;
-        }
 
-        return false;
+
+    public boolean remove(String rotulo){
+        dados.remove(rotulo);
+        return true;
     }
 
-    public String formata(String mascara, String valor) {
-        MaskFormatter mask = null;
-        String resultado = "";
-        try {
-            mask = new MaskFormatter(mascara);
-            mask.setValueContainsLiteralCharacters(false);
-            mask.setPlaceholderCharacter('_');
-            resultado = mask.valueToString(valor);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return resultado;
-    }
+
+
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        dados.forEach((rotulo, telefone) ->
-                sb.append(rotulo)
-                        .append(": +")
-                        .append(formata("##(##)#####-###", telefone))
-                        .append("\n")
-        );
-
-        return sb.toString();
+            StringBuilder sb = new StringBuilder();
+            dados.forEach((rotulo, email) -> sb.append(rotulo).append(": ").append(email).append("\n"));
+            return sb.toString();
     }
 }
+
+
+
+
+
+
+
