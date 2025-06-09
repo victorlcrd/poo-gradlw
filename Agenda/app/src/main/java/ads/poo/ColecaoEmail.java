755726162558be7a8742691d17b0716f0b22ab10
Victor Lacerda <163481651+victorlcrd@users.noreply.github.com>
2025-06-09 18:43:31 -0300

    Update ColecaoEmail.java

--- Agenda/app/src/main/java/ads/poo/ColecaoEmail.java
+++ Agenda/app/src/main/java/ads/poo/ColecaoEmail.java
@@ -1,36 +1,53 @@
-package main.java.ads.poo;
+package ads.poo;
 
 import java.util.HashMap;
 
 public class ColecaoEmail {
-    private HashMap<String, String> colecaoEmail;
 
-    public ColecaoEmail() {
-        colecaoEmail = new HashMap<>();
-    }
+    private HashMap<String,String> dados = new HashMap<>();
 
-    public boolean add(String rotulo, String email){
-        colecaoEmail.put(rotulo, email);
-        return colecaoEmail.containsKey(rotulo);
-    }
 
-    public boolean remove(String rotulo){
-        colecaoEmail.entrySet().removeIf(email -> email.getKey().equals(rotulo));
-        return !colecaoEmail.containsKey(rotulo);
-    }
-
-    public boolean update(String rotulo, String valor){
-        if (!colecaoEmail.containsKey(rotulo)){
+    public boolean add(String email, String rotulo){
+        String eR = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+        if(email.matches(eR)) {
+            dados.put(rotulo, email);
+            return true;
+        } else {
             return false;
         }
-        colecaoEmail.replace(rotulo, valor);
+    }
+
+    public boolean update(String email, String rotulo) {
+        String eR = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+        if (email.matches(eR) && dados.get(email) == null) {
+            dados.put(rotulo, email);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    public boolean remove(String rotulo){
+        dados.remove(rotulo);
         return true;
     }
 
+
+
+
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        colecaoEmail.forEach((chave, valor) -> sb.append("    - " + chave + ": " + valor + "\n"));
-        return sb.toString();
+            StringBuilder sb = new StringBuilder();
+            dados.forEach((rotulo, email) -> sb.append(rotulo).append(": ").append(email).append("\n"));
+            return sb.toString();
     }
 }
+
+
+
+
+
+
+
